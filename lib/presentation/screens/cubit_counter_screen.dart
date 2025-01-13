3184a8fe7e3e993fdde88c52b3200bd27208b927
gB3rnard <classingnow@gmail.com>
2025-01-13 17:45:45 +0100

    botones añadidos a la pantalla de countercubits

--- lib/presentation/screens/cubit_counter_screen.dart
+++ lib/presentation/screens/cubit_counter_screen.dart
@@ -9,10 +9,48 @@ class CubitCounterScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Cubit Counter'),
+        actions: [
+          IconButton(
+            onPressed: () => {}, 
+            icon: const Icon(Icons.refresh_outlined
+          ),
+          )
+        ],
       ),
       body: const Center(
         child: Text('Counter value: xxxx'),
         
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            heroTag: '1',
+            child: const Text('+3'),
+            onPressed: () => {}
+          ), 
+
+          const SizedBox(
+            height: 15,
+          ),
+
+          FloatingActionButton(
+            heroTag: '2',
+            child: const Text('+2'),
+            onPressed: () => {}
+          ), 
+
+          const SizedBox(
+            height: 15,
+          ), 
+
+          FloatingActionButton(
+            heroTag: '3',
+            child: const Text('+1'),
+            onPressed: () => {}
+          ), 
+        
+        ],
       ),
     );
   }
